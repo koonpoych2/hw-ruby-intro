3cd0586b7966d74a75a06bc0d2e460cd469391e0
Anecha Khumjun <68006281+koonpoych2@users.noreply.github.com>
2023-07-24 00:40:02 +0700

    done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -28,14 +28,15 @@ def max_2_sum arr
 end
 
 def sum_to_n? arr, n
-  return true if arr.empty? && n.zero?
+  return false if arr.empty? && n.zero?
   arr.combination(2).any? {|a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
- puts "Hello, %s" %[name]
+  "Hello, #{name}"
+  
 end
 
 def starts_with_consonant?(s)
@@ -54,10 +55,31 @@ end
 
 def binary_multiple_of_4? s
 
+  # Check if the string is empty or contains non-binary characters
+  return false unless s.match?(/^[01]+$/)
+
+  # Convert the binary string to an integer and check if it's a multiple of 4
+  number = s.to_i(2)
+  number % 4 == 0
+
 end
 
 # Part 3
 
 class BookInStock
+
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError, 'ISBN number cannot be empty' if isbn.empty?
+    raise ArgumentError, 'Price must be greater than zero' if price <= 0
+
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format('$%.2f', price)
+  end
 
 end
