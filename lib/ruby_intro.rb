664a0833ca05cca2d48322f3a28f2cd0032c3688
Anecha Khumjun <68006281+koonpoych2@users.noreply.github.com>
2023-07-23 11:22:56 +0000

    update

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,11 +4,29 @@
 
 def sum arr
   # YOUR CODE HERE
-  arr.each {}
+  result = 0
+  arr.each { |element| result += element  }
+  result
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  
+  # If the array is empty, return zero
+  return 0 if arr.empty?
+
+  # If the array has only one element, return that element
+  return arr[0] if arr.length == 1
+
+  # Sort the array in descending order to get the two largest elements at the beginning
+  sorted_arr = arr.sort.reverse
+
+  # Calculate the sum of the two largest elements
+  sum_of_two_largest = sorted_arr[0] + sorted_arr[1]
+
+  # Return the sum
+  sum_of_two_largest
+
 end
 
 def sum_to_n? arr, n
